1f72c5d89dd4a5234123f34715ec46c5cef83c1f
1337777 <OOO1337777@outlook.com>
2025-02-27 16:05:41 +0800

    Updates

---
+++ assets/browser-C-9PyCEu.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-vV3IG1-f.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
